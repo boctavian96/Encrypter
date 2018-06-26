6be61f36a775e616c3b5a0dcb4df57493025ec4f
octavian <boctavian96@yahoo.com>
2018-06-26 23:55:56 +0300

    Xml

--- src/octavian/filemanager/xml/XmlAbstract.java
+++ src/octavian/filemanager/xml/XmlAbstract.java
@@ -5,10 +5,29 @@
  */
 package octavian.filemanager.xml;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+
 /**
  *
  * @author octavian
  */
 public abstract class XmlAbstract {
+    
+    private DocumentBuilderFactory documentFactory;
+    private DocumentBuilder documentBuilder;
+    private Document document;
+    
+    public XmlAbstract(){
+        try{
+            this.documentFactory = DocumentBuilderFactory.newInstance();
+            this.documentBuilder = documentFactory.newDocumentBuilder();
+        }catch(ParserConfigurationException e){
+            
+        }
+    }
+    
     public abstract void execute();
 }
--- src/octavian/filemanager/xml/XmlWriter.java
+++ src/octavian/filemanager/xml/XmlWriter.java
@@ -10,9 +10,7 @@ package octavian.filemanager.xml;
  * @author octavian
  */
 public class XmlWriter extends XmlAbstract{
-
-    
-    
+   
     @Override
     public void execute() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
